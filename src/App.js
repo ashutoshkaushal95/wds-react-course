23a1fd71c6027a649784c3bc79e8cf3f16e48a0e
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-12-04 16:45:38 +0530

    button color changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CounterHooks from "./CounterHooks";
 export const ThemeContext = React.createContext()
 
 function App() {
-  const [theme, setTheme] = useState('red')
+  const [theme, setTheme] = useState('green')
   return (
     <ThemeContext.Provider value={{ backgroundColor: theme }}>
       Class Counter
